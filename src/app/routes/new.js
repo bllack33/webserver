504f73ed6031c3b684081af0481bb5b3c9762a95
juandavid <bllack33@gmail.com>
2020-08-25 12:02:29 -0500

    new

--- src/app/routes/new.js
+++ src/app/routes/new.js
@@ -26,7 +26,7 @@ module.exports = app => {
         "https://www.amazon.com/-/es/Libbey-Mixologist-9-Piece-Cocktail-Set/dp/",
     ]
     app.get('/', (req, res) => {
-        connection.query('SELECT * FROM product ORDER BY id DESC', (error, result) => {
+        connection.query('SELECT * FROM product', (error, result) => {
             // console.log(result);
             res.render('news/news.ejs', {
                 product: result
@@ -35,7 +35,8 @@ module.exports = app => {
     });
 
     app.post('/product', (req, res) => {
-        const { asin_code } = req.body;
+        let { asin_code } = req.body;
+        asin_code = asin_code.trim();
         var item = user_agent[Math.floor(Math.random() * user_agent.length)];
         var links = urls[Math.floor(Math.random() * urls.length)];
         const url = links + asin_code;
@@ -52,14 +53,16 @@ module.exports = app => {
                 let data = await page.evaluate(() => {
                     let title = document.querySelector('#productTitle') ? document.querySelector('#productTitle').innerText : "";
                     let imagen = document.querySelectorAll('.imgTagWrapper img.a-dynamic-image') ? document.querySelectorAll('.imgTagWrapper img.a-dynamic-image')[0].src : "";
+                    let description = document.querySelector('#feature-bullets').textContent ?document.querySelector('#feature-bullets').textContent.trim():"";
                     let precio = document.querySelector('.a-span12 #priceblock_ourprice') ? document.querySelector('.a-span12 #priceblock_ourprice').innerText : "";
-                    let imagen_dos = document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img') ? document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img').src : "";
+                    let imagen_dos = document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img')?document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img').src:"";
                     let peso_prod = "";
-
+                    description = description.replace('P.when("ReplacementPartsBulletLoader").execute(function(module){ module.initializeDPX(); })', '').trim();
                     title = title.replace('Amazon', 'Tienda').trim();
                     precio = precio.replace('US$', '').trim();
                     return {
                         title,
+                        description,
                         imagen,
                         imagen_dos,
                         precio,
@@ -70,10 +73,11 @@ module.exports = app => {
                 console.log(data);
                 await browser.close();
 
-                connection.query('SELECT * FROM product WHERE asin_code =' + "'" + asin_code + "LIMIT 1'", (error, result) => {
-                    if (result) {
+                connection.query('SELECT * FROM product WHERE asin_code =' + "'" + asin_code + "';", (error, result) => {
+                    if (result.length > 0) {
                         connection.query('UPDATE product SET? WHERE asin_code=' + "'" + asin_code + "'", {
                             name: data.title,
+                            description: data.description,
                             img: data.imagen,
                             img2: data.imagen_dos,
                             cost: data.precio,
@@ -83,11 +87,11 @@ module.exports = app => {
                         }), (err, result) => {
                             res.redirect('/');
                         }
-
                     } else {
                         connection.query('INSERT INTO product SET?', {
                             asin_code: asin_code,
                             name: data.title,
+                            description: data.description,
                             img: data.imagen,
                             img2: data.imagen_dos,
                             cost: data.precio,
@@ -135,14 +139,16 @@ module.exports = app => {
                             let data = await page.evaluate(() => {
                                 let title = document.querySelector('#productTitle') ? document.querySelector('#productTitle').innerText : "";
                                 let imagen = document.querySelectorAll('.imgTagWrapper img.a-dynamic-image') ? document.querySelectorAll('.imgTagWrapper img.a-dynamic-image')[0].src : "";
+                                let description = document.querySelector('#feature-bullets').textContent ?document.querySelector('#feature-bullets').textContent.trim():"";
                                 let precio = document.querySelector('.a-span12 #priceblock_ourprice') ? document.querySelector('.a-span12 #priceblock_ourprice').innerText : "";
-                                let imagen_dos = document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img') ? document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img').src : "";
+                                let imagen_dos = document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img')?document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img').src:"";
                                 let peso_prod = "";
-
+                                description = description.replace('P.when("ReplacementPartsBulletLoader").execute(function(module){ module.initializeDPX(); })', '').trim();
                                 title = title.replace('Amazon', 'Tienda').trim();
                                 precio = precio.replace('US$', '').trim();
                                 return {
                                     title,
+                                    description,
                                     imagen,
                                     imagen_dos,
                                     precio,
@@ -153,30 +159,31 @@ module.exports = app => {
                             console.log(data.title ? "exito" : "fail");
                             await browser.close();
 
-                            connection.query('SELECT * FROM product WHERE asin_code =' + "'" + asin_code + "LIMIT 1'", (error, result) => {
-                                if (result) {
+                            connection.query('SELECT * FROM product WHERE asin_code =' + "'" + asin_code + "';", (error, result) => {
+                                if (result.length > 0) {
                                     connection.query('UPDATE product SET? WHERE asin_code=' + "'" + asin_code + "'", {
                                         name: data.title,
+                                        description: data.description,
                                         img: data.imagen,
                                         img2: data.imagen_dos,
                                         cost: data.precio,
                                         shipping_weight: data.peso_prod,
                                         active: data.precio ? 1 : 0,
-
+            
                                     }), (err, result) => {
                                         res.redirect('/');
                                     }
-
                                 } else {
                                     connection.query('INSERT INTO product SET?', {
                                         asin_code: asin_code,
                                         name: data.title,
+                                        description: data.description,
                                         img: data.imagen,
                                         img2: data.imagen_dos,
                                         cost: data.precio,
                                         shipping_weight: data.peso_prod,
                                         active: data.precio ? 1 : 0,
-
+            
                                     }), (err, result) => {
                                         res.redirect('/');
                                     }
@@ -201,150 +208,92 @@ module.exports = app => {
 
     app.post('/pendingproduct', (req, res) => {
 
-        connection.query('SELECT asin_code FROM product WHERE NAME = "" OR NAME IS NULL;', (error, result) => {
-            function playRecording3() {
-                for (var i = 0; i < result.length; i++) {
-                    try {
-                        var item = user_agent[Math.floor(Math.random() * user_agent.length)];
-                        playNote(result[i].asin_code, i, item);
-                    } catch (error) {
-                        console.log("error juan:", error.message);
-                    }
+        connection.query('SELECT * FROM product', (error, result) => {
+            console.log(result.length);
 
+            function playRecording() {
+                for (var i = 0; i < result.length; i++) {
+                    var item = user_agent[Math.floor(Math.random() * user_agent.length)];
+                    playNote(result[i].asin_code, i, item);
                 };
             }
 
-            async function playNote(asin_code, i, item) {
-                setTimeout(async function() {
-                    console.log(asin);
-                    let browser = await puppeteer.launch();
-                    let page = await browser.newPage();
-                    await page.setUserAgent(item);
-                    await page.goto(url, { waitUntil: "networkidle2" });
-                    const userAgent = await page.evaluate(() => navigator.userAgent);
-                    console.log(userAgent);
-                    try {
-                        let data = await page.evaluate(() => {
-                            let title = document.querySelector('#productTitle') ? document.querySelector('#productTitle').innerText : "";
-                            let imagen = document.querySelectorAll('.imgTagWrapper img.a-dynamic-image') ? document.querySelectorAll('.imgTagWrapper img.a-dynamic-image')[0].src : "";
-                            let precio = document.querySelector('.a-span12 #priceblock_ourprice') ? document.querySelector('.a-span12 #priceblock_ourprice').innerText : "";
-                            let imagen_dos = document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img') ? document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img').src : "";
-                            let peso_prod = "";
+            function playNote(asin_code, i, item) {
+                setTimeout(function() {
+                    (async() => {
+                        console.log(asin_code);
+                        var links = urls[Math.floor(Math.random() * urls.length)];
+                        const url = links + asin_code;
+                        let browser = await puppeteer.launch();
+                        let page = await browser.newPage();
+                        await page.setDefaultNavigationTimeout(0);
+                        await page.setUserAgent(item);
+                        await page.goto(url, { waitUntil: "networkidle2" });
+                        const userAgent = await page.evaluate(() => navigator.userAgent);
+                        try {
+                            let data = await page.evaluate(() => {
+                                let title = document.querySelector('#productTitle') ? document.querySelector('#productTitle').innerText : "";
+                                let imagen = document.querySelectorAll('.imgTagWrapper img.a-dynamic-image') ? document.querySelectorAll('.imgTagWrapper img.a-dynamic-image')[0].src : "";
+                                let precio = document.querySelector('.a-span12 #priceblock_ourprice') ? document.querySelector('.a-span12 #priceblock_ourprice').innerText : "";
+                                let imagen_dos = document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img') ? document.querySelector('#main-image-container > ul > li.image.item.itemNo1.maintain-height.selected > span > span > div > img').src : "";
+                                let peso_prod = "";
 
-                            title = title.replace('Amazon', 'Tienda').trim();
-                            precio = precio.replace('US$', '').trim();
-                            return {
-                                title,
-                                imagen,
-                                imagen_dos,
-                                precio,
-                                peso_prod,
-                            }
+                                title = title.replace('Amazon', 'Tienda').trim();
+                                precio = precio.replace('US$', '').trim();
+                                return {
+                                    title,
+                                    imagen,
+                                    imagen_dos,
+                                    precio,
+                                    peso_prod,
+                                }
 
-                        });
-                        console.log(data);
-                        await browser.close();
-                    } catch (error) {
-                        console.log(error);
-                        res.redirect('/');
-                    }
-                }, 15000 * i);
-            }
-            playRecording3();
+                            });
+                            console.log(data.title ? "exito" : "fail");
+                            await browser.close();
 
-            res.redirect('/');
+                            connection.query('SELECT * FROM product WHERE asin_code =' + "'" + asin_code + "';", (error, result) => {
+                                if (result.length > 0) {
+                                    connection.query('UPDATE product SET? WHERE asin_code=' + "'" + asin_code + "'", {
+                                        name: data.title,
+                                        img: data.imagen,
+                                        img2: data.imagen_dos,
+                                        cost: data.precio,
+                                        shipping_weight: data.peso_prod,
+                                        active: data.precio ? 1 : 0,
+            
+                                    }), (err, result) => {
+                                        res.redirect('/');
+                                    }
+                                } else {
+                                    connection.query('INSERT INTO product SET?', {
+                                        asin_code: asin_code,
+                                        name: data.title,
+                                        img: data.imagen,
+                                        img2: data.imagen_dos,
+                                        cost: data.precio,
+                                        shipping_weight: data.peso_prod,
+                                        active: data.precio ? 1 : 0,
+            
+                                    }), (err, result) => {
+                                        res.redirect('/');
+                                    }
+                                }
+                            });
+                            res.redirect('/');
 
-        }), (err, result) => {
-            console.log(err);
-        }
-    });
-
-    app.post('/pendingproduct1', (req, res) => {
-
-        const putRequest = async(url, userAgent) => {
-            try {
-                request({
-                        url: url,
-                        method: "GET",
-                        headers: {
-                            'User-Agent': userAgent
-                        },
-                    },
-                    (err, response, body) => {
-                        if (err) {
-                            return {
-                                status: 400,
-                                error: err,
+                        } catch (e) {
+                            if (e instanceof puppeteer.errors.TimeoutError) {
+                                console.log("no carga");
                             }
                         }
-                        return {
-                            body,
-                            status: 200,
-                        };
-                    }
-                );
-            } catch (err) {
-                return {
-                    status: 400,
-                    error: err.message,
-                }
-            }
-        };
-
-        connection.query('SELECT asin_code FROM product WHERE NAME = "" OR NAME IS NULL;', (error, result) => {
-            function playRecording3() {
-                for (var i = 0; i < result.length; i++) {
-                    try {
-                        var item = user_agent[Math.floor(Math.random() * user_agent.length)];
-                        playNote(result[i].asin_code, i, item);
-                    } catch (error) {
-                        console.log("error juan:", error.message);
-                    }
-
-                };
-            }
-
-            async function playNote(asin, i, item) {
-                setTimeout(async function() {
-                    console.log(asin);
-                    const url = 'https://www.amazon.com/dp/' + asin;
-                    const response = await putRequest(url, item);
-
-
-                    if (response !== undefined && response !== "undefined") {
-                        console.log("entro");
-                        let $ = cheerio.load(response);
-                        let title = $('#productTitle').text().trim();
-                        let imagen = $('#imgTagWrapperId img').attr('src');
-                        let precio = $('.a-span12 #priceblock_ourprice') ? $('.a-span12 #priceblock_ourprice').text() : "";
-                        let peso_prod = $('#productDetails_detailBullets_sections1 > tbody > tr:nth-child(2) > td').text();
-
-                        peso_prod = peso_prod.replace(/[^0-9|\.]/g, '').trim();
-                        precio = precio.replace('US$', '').trim();
-
-                        connection.query('UPDATE product SET? WHERE asin_code=' + "'" + asin + "'", {
-                            name: title,
-                            img: imagen,
-                            cost: precio,
-                            shipping_weight: peso_prod,
-                            active: precio ? 1 : 0,
-
-                        }), (err, result) => {
-                            console.log(err.StatusCodeError);
-                        }
-                    } else {
-                        console.log("respuesta", response);
-                    };
-
-
+                    })();
                 }, 15000 * i);
             }
-            playRecording3();
+            playRecording()
+
 
             res.redirect('/');
-
-        }), (err, result) => {
-            console.log(err);
-        }
+        })
     });
 }
